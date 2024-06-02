2e21d9042436a6fea8c247c1fdf3c2c3c84feb14
DANG BA MINH <d.baminh@gmail.com>
2024-06-02 10:31:09 +0700

    feat: add debug file

--- Codeforces/template.cpp
+++ Codeforces/template.cpp
@@ -1,7 +1,10 @@
 #include <bits/stdc++.h>
+//#include "debug.cpp"
+//define LOCAL
 #define REP(i,a,b) for(int i=a; i<=b; i++)
 typedef long long ll;
 using namespace std;
+
 int main() {
     // Fast I/O
     ios::sync_with_stdio(0);
