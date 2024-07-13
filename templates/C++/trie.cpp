4ffece1eac73460c40c2c7d0b390283c15dcc3f9
DANG BA MINH <d.baminh@gmail.com>
2024-07-13 18:01:22 +0700

    feat: update trie.cpp

---
+++ templates/C++/trie.cpp
@@ -0,0 +1,45 @@
+#include <string>
+using namespace std;
+
+struct TrieNode {
+  unordered_map<char, TrieNode *> children{};
+  bool is_word = false;
+};
+
+class Trie {
+private:
+  TrieNode *root;
+
+public:
+  Trie() { root = new TrieNode(); }
+
+  void insert(string word) {
+    auto p = root;
+    for (const auto &c : word) {
+      if (p->children.count(c) == 0)
+        p->children[c] = new TrieNode();
+      p = p->children[c];
+    }
+    p->is_word = true;
+  }
+
+  bool search(string word) {
+    auto p = root;
+    for (const auto &c : word) {
+      if (p->children.count(c) == 0)
+        return false;
+      p = p->children[c];
+    }
+    return p->is_word;
+  }
+
+  bool startsWith(string prefix) {
+    auto p = root;
+    for (const auto &c : prefix) {
+      if (p->children.count(c) == 0)
+        return false;
+      p = p->children[c];
+    }
+    return true;
+  }
+};
