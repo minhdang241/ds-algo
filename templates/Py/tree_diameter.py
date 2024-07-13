feb3eceb112d71e06c62c4bd5a7a8c39d6c9f8c5
DANG BA MINH <d.baminh@gmail.com>
2024-07-14 06:58:58 +0700

    feat: add tree diameter template

---
+++ templates/Py/tree_diameter.py
@@ -0,0 +1,31 @@
+from collections import defaultdict
+from typing import List
+
+def treeDiameter(edges: List[List[int]]) -> int:
+    G = defaultdict(list)
+
+    for a, b in edges:
+        G[a].append(b)
+        G[b].append(a)
+
+    max_val = 0
+    f_node = 0
+    visited = set()
+
+    def dfs(node, visited, G, l):
+        nonlocal max_val, f_node
+        visited.add(node)
+
+        if l > max_val:
+            max_val = l
+            f_node = node
+
+        for neighbor in G[node]:
+            if neighbor not in visited:
+                dfs(neighbor, visited, G, l + 1)
+
+    dfs(0, visited, G, 0)
+    
+    visited = set()
+    dfs(f_node, visited, G, 0)
+    return max_val
