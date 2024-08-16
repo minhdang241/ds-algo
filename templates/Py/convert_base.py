4ed451ce8577b393d481940471f4abcfcd297285
DANG BA MINH <d.baminh@gmail.com>
2024-08-16 14:32:39 +0700

    feat: add convert_base template

---
+++ templates/Py/convert_base.py
@@ -0,0 +1,15 @@
+def to_base(n, base):
+    ans = []
+    while n:
+        n, r = divmod(n, base)
+        if r < 0:
+            r += -base
+            n += 1
+        ans.append(r)
+    return ans[::-1] or [0]
+
+
+if __name__ == "__main__":
+    ans = to_base(-3, -2)
+    assert ans == [1,1,0,1]
+
