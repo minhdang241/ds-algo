c27620036726f8351ad4a879d91f9349e8dd166c
DANG BA MINH <d.baminh@gmail.com>
2024-10-07 11:16:12 +1100

    feat: add test files

---
+++ templates/C++/string_test.cpp
@@ -0,0 +1,9 @@
+#include<stdio.h>
+#include<string>
+#include<iostream>
+
+int main(void) {
+    std::string s = "abc";
+    std::cout << s.substr(1) << std::endl;
+    return 0;
+}
---
+++ templates/Py/test.py
@@ -0,0 +1,11 @@
+class A:
+    def __init__(self, d={}):
+        self.d = d
+
+if __name__ == "__main__":
+    a = A()
+    b = A()
+    b.d["key"] = "value"
+    assert(a.d["key"] == "value")
+
+
