1432009b049266e7884ee6b6093a51e83f829e50
DANG BA MINH <d.baminh@gmail.com>
2024-11-19 11:17:14 +1100

    feat: update segment tree template

--- templates/Py/segment_tree.py
+++ templates/Py/segment_tree.py
@@ -1,35 +1,64 @@
-class SegmentTree:
-    def __init__(self, arr):
-        n = len(arr)
-        self.nodes = [0 for _ in range(4*n)]
-        self._build(0, arr, 0, len(arr)-1)
-        self.arr = arr
+from __future__ import annotations
 
-    def _build(self, node, arr, start, end):
-        if start == end:
-            self.nodes[node] = arr[start]
-        else:
-            mid = (start + end) // 2
-            self._build(2*node+1, arr, start, mid)
-            self._build(2*node+2, arr, mid+1, end)
-            self.nodes[node] = self.nodes[2*node+1] + self.nodes[2*node+2]
 
-    def query(self, node, start, end, l,r):
-        if r < start or l > end:
-            return 0
-        if l <= start and end <= r:
-            return self.nodes[node]
-        mid = (start+end) // 2 
-        return self.query(2*node+1, start, mid, l, r) + self.query(2*node+2, mid+1, end, l, r)
-    
-    def update(self, idx, node, start, end, new_val):
-        if start == end:
-            self.arr[idx] = new_val
-            self.nodes[node] = new_val
+class SegTree:
+    def __init__(self, t):
+        self.t = t
+
+    def build(self, a, v, tl, tr):
+        t = self.t
+        if tl == tr:
+            t[v] = a[tl]
         else:
-            mid = (start+end) // 2
-            if start <= idx <= mid:
-                self.update(idx, 2*node+1, start, mid, new_val)
+            tm = (tl + tr) // 2
+            self.build(a, v * 2, tl, tm)
+            self.build(a, v * 2 + 1, tm + 1, tr)
+            t[v] = min(t[v * 2], t[v * 2 + 1])
+
+    def query(self, v, l, r, tl, tr):
+        t = self.t
+        if l > r:
+            return INF
+        if l == tl and r == tr:
+            return t[v]
+
+        tm = (tl + tr) // 2
+        return min(
+            self.query(v * 2, l, min(r, tm), tl, tm),
+            self.query(v * 2 + 1, max(l, tm + 1), r, tm + 1, tr),
+        )
+
+    def update(self, v, tl, tr, pos, new_val):
+        t = self.t
+        if tl == tr:
+            t[v] = new_val
+        else:
+            tm = (tl + tr) // 2
+            if pos <= tm:
+                self.update(v * 2, tl, tm, pos, new_val)
             else:
-                self.update(idx, 2*node+2, mid+1, end, new_val)
-            self.nodes[node] = self.nodes[2*node+1] + self.nodes[2*node+2]
+                self.update(v * 2 + 1, tm + 1, tr, pos, new_val)
+            t[v] = min(t[v * 2], t[v * 2 + 1])
+
+    def __str__(self):
+        return str(self.t)
+
+
+if __name__ == "__main__":
+    li = [3, 2, 4, 5, 1, 1, 5, 3]
+    queries = [[2, 1, 4], [2, 5, 6], [1, 2, 3], [2, 1, 4]]
+    N = len(li)
+    INF = float("inf")
+    t = [INF] * (4 * N)
+    segtree = SegTree(t)
+    segtree.build(li, 1, 0, N - 1)
+    print(segtree)
+    for q in queries:
+        ty, a, b = q
+        if ty == 1:
+            a -= 1
+            segtree.update(1, 0, N - 1, a, b)
+        else:
+            a -= 1
+            b -= 1
+            print(segtree.query(1, a, b, 0, N - 1))
