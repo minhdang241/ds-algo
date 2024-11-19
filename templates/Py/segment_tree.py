06cf6024648acd9b86c94a45eb9f0d73581b74b9
DANG BA MINH <d.baminh@gmail.com>
2024-11-19 13:52:22 +1100

    feat: update segment tree template

--- templates/Py/segment_tree.py
+++ templates/Py/segment_tree.py
@@ -13,17 +13,17 @@ class SegTree:
             tm = (tl + tr) // 2
             self.build(a, v * 2, tl, tm)
             self.build(a, v * 2 + 1, tm + 1, tr)
-            t[v] = min(t[v * 2], t[v * 2 + 1])
+            t[v] = max(t[v * 2], t[v * 2 + 1])
 
     def query(self, v, l, r, tl, tr):
         t = self.t
         if l > r:
-            return INF
+            return 0
         if l == tl and r == tr:
             return t[v]
 
         tm = (tl + tr) // 2
-        return min(
+        return max(
             self.query(v * 2, l, min(r, tm), tl, tm),
             self.query(v * 2 + 1, max(l, tm + 1), r, tm + 1, tr),
         )
@@ -38,15 +38,16 @@ class SegTree:
                 self.update(v * 2, tl, tm, pos, new_val)
             else:
                 self.update(v * 2 + 1, tm + 1, tr, pos, new_val)
-            t[v] = min(t[v * 2], t[v * 2 + 1])
+            t[v] = max(t[v * 2], t[v * 2 + 1])
 
     def __str__(self):
         return str(self.t)
 
 
 if __name__ == "__main__":
-    li = [3, 2, 4, 5, 1, 1, 5, 3]
-    queries = [[2, 1, 4], [2, 5, 6], [1, 2, 3], [2, 1, 4]]
+    li = [2, 4, 2, 5, 6]
+    # queries = [[2, 1, 4], [2, 5, 6], [1, 2, 3], [2, 1, 4]]
+    queries = [[2, 1, 4], [2, 1, 1], [2, 1, 2]]
     N = len(li)
     INF = float("inf")
     t = [INF] * (4 * N)
@@ -62,3 +63,4 @@ if __name__ == "__main__":
             a -= 1
             b -= 1
             print(segtree.query(1, a, b, 0, N - 1))
+    print(segtree)
