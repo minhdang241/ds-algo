6a26cbb1b4c30b9cc701bef437a3127606cffd19
DANG BA MINH <d.baminh@gmail.com>
2025-01-29 10:11:27 +0700

    feat: update union_find

--- templates/Py/union_find.py
+++ templates/Py/union_find.py
@@ -1,26 +1,26 @@
 class UnionFind:
   def __init__(self, size):
-    self.rank = [1 for _ in range(size)]
-    self.root = [i for i in range(size)]
+    self.ranks = [1 for _ in range(size)]
+    self.roots = [i for i in range(size)]
 
   def find(self, x):
-    if x == self.root[x]:
+    if x == self.roots[x]:
       return x
-    self.root[x] = self.find(self.root[x])
-    return self.root[x]
+    self.roots[x] = self.find(self.roots[x])
+    return self.roots[x]
 
   def union(self, x, y):
     root_x, root_y = self.find(x), self.find(y)
     if root_x == root_y:
       return
 
-    if self.rank[root_x] > self.rank[root_y]:
+    if self.ranks[root_x] > self.ranks[root_y]:
       self.root[root_y] = root_x
-    elif self.rank[root_x] < self.rank[root_y]:
-      self.root[root_x] = root_y
+    elif self.ranks[root_x] < self.ranks[root_y]:
+      self.roots[root_x] = root_y
     else:
-      self.root[root_y] = root_x
-      self.rank[root_x] += 1
+      self.roots[root_y] = root_x
+      self.ranks[root_x] += 1
 
   def connected(self, x, y):
     return self.find(x) == self.find(y)
