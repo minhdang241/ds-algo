792cf767e9b7cd1cd8b3d9c5568581b515a398f3
DANG BA MINH <d.baminh@gmail.com>
2025-02-02 14:53:44 +0700

    feat: update BIT template

--- templates/Py/bit.py
+++ templates/Py/bit.py
@@ -3,29 +3,32 @@
 
 class BIT:
   def __init__(self, arr):
-    self.tree = [0] + arr
-    self.size = len(self.tree)
-    for i in range(1, self.size):
-      pi = i + (i & -i)
-      if pi < self.size:
-        self.tree[pi] += self.tree[i]
+    self.size = len(arr) + 1
+    self.tree = [0] * self.size
+    for i in range(len(arr)):
+      self.add(i, arr[i])
 
   def add(self, pos, val):
+    pos += 1
     while pos < self.size:
       self.tree[pos] += val
-      pos += pos & -pos
+      pos = pos + (pos & (-pos))
 
   def sum(self, pos):
+    pos += 1
     res = 0
     while pos > 0:
       res += self.tree[pos]
-      pos -= pos & -pos
+      pos = pos - (pos & (-pos))
     return res
+
+  def range_sum(self, l, r):
+    return self.sum(r) - self.sum(l - 1)
 
 
 if __name__ == "__main__":
   arr = [5, 2, 9, -3, 5, 20, 10, -7, 2, 3, -4, 0, -2, 15, 5]
   bit = BIT(arr)
-  assert bit.sum(7) == 48
+  assert bit.sum(6) == 48
   bit.add(4, 10)
-  assert bit.sum(8) == 51
+  assert bit.sum(7) == 51
