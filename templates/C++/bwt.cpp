a4be814231199d8bfbb85843d75e5657aba100f6
minhdang241 <d.baminh@gmail.com>
2025-12-01 10:58:23 +1100

    feat: add optimal way

--- templates/C++/bwt.cpp
+++ templates/C++/bwt.cpp
@@ -1,9 +1,10 @@
-
-
+#include <unordered_map>
+#include <map>
 #include <vector>
 #include <string>
 #include <iostream>
 #include <sstream>
+#include <cassert>
 
 auto bwt_encode(std::string s) -> std::string {
     auto N = s.size();
@@ -32,13 +33,6 @@ auto bwt_encode(std::string s) -> std::string {
 
 }
 
-auto print(std::vector<char> vec) {
-    for (const auto& c: vec) {
-        std::cout << c << "";
-    }
-    std::cout << "\n";
-
-}
 auto bwt_decode(std::string s) -> std::string {
     auto N = s.size();
     auto table = std::vector<std::vector<char>>(N, std::vector<char>(N, '\0'));
@@ -60,11 +54,67 @@ auto bwt_decode(std::string s) -> std::string {
     return "";
 }
 
+auto bwt_decode2(std::string s) -> std::string {
+    auto N = s.size();
+    auto first_column = s;
+    std::sort(first_column.begin(), first_column.end());
+
+    auto cTable = std::map<char, int>();
+    for (const auto& c: first_column) {
+        cTable[c]++;
+    }
+
+    auto cumulative_count = 0;
+    for (auto& [c, count]: cTable) {
+        auto current_count = count;
+        count = cumulative_count;
+        cumulative_count += current_count;
+    }
+
+    auto current_index = 0;
+    for (auto i = 0; i < N; i++) {
+        const auto c = s[i];
+        if (c == '#') {
+            current_index = i;
+            break;
+        }
+    }
+
+    auto rankTable = std::vector<std::unordered_map<char, int>>(N + 1);
+    for (auto i = 0; i < N; i++) {
+        rankTable[i + 1] = rankTable[i];
+        rankTable[i + 1][s[i]]++;
+    }
+
+    auto decoded_string = std::string();
+    decoded_string.reserve(N);
+    for (auto i = 0; i < N; i++) {
+        const auto c = s[current_index];
+        decoded_string += c;
+        auto rank = rankTable[current_index][c];
+        current_index = cTable[c] + rank;
+    }
+    std::reverse(decoded_string.begin(), decoded_string.end());
+    return decoded_string;
+}
+
+auto print(std::vector<char> vec) {
+    for (const auto& c: vec) {
+        std::cout << c << "";
+    }
+    std::cout << "\n";
+
+}
+
 int main() {
     auto s = "BABANAS#";
     std::cout << "Original: " << s << "\n";
-    auto ret = bwt_encode(s);
-    std::cout << "Encode: " << ret << "\n";
-    auto dec_ret = bwt_decode(ret);
-    std::cout << "Decode: " << dec_ret << "\n";
+    auto enc = bwt_encode(s);
+    std::cout << "Encode: " << enc << "\n";
+    auto dec = bwt_decode(enc);
+    assert(dec == s);
+    std::cout << "Decode: " << dec << "\n";
+    auto dec2 = bwt_decode2(enc);
+    std::cout << "Decode2: " << dec2 << "\n";
+    assert(dec == dec2);
 }
